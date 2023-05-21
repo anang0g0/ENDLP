c6997c4ebb5c04db34e719b7dd004ce9160ec931
Osamu Sakai <fumumue@protonmail.com>
2023-05-21 22:49:23 +0900

    longlong

--- mini.c
+++ mini.c
@@ -124,17 +124,26 @@ int p0w(int c, int n)
 	return d;
 }
 
+typedef union {
+unsigned char c[8];
+unsigned short s[4];
+unsigned int x[2];
+unsigned long long u;
+} UI;
+
 unsigned int period = 0, counti = 0;
-unsigned char slf(unsigned char l)
+unsigned long long slf(unsigned l)
 {
 	unsigned char lfs = l; // t=g(a)
 	unsigned char m = lfs;
 	unsigned int kount = 0;
+	UI u;
+
 	FILE *fp;
 	int i = 1;
 	counti = be(lfs);
 	fp = fopen("test.bin", "wb");
-	while (i < 200000000)
+	while (i < 64)
 	{
 		unsigned char lfs2 = lfsr(lfs2 ^ lfs);
 
@@ -143,12 +152,13 @@ unsigned char slf(unsigned char l)
 		lfs ^= (p0w(loo(Dot(lfs, lfs2)), (counti >> 1) + 1)); // s=(A^2r^2)^n
 		lfs ^= (Dot(lfs, (loo(m) ^ be(m) ^ c)));			  // s^n(A^2t+u) = s^n(A^2t+(At+c))
 		++period;
+		u.c[i%8]^=lfs;
 		// printf("%d %d\n", lfs, period);
-		fwrite(&lfs, sizeof(lfs), 1, fp);
+		//fwrite(&lfs, sizeof(lfs), 1, fp);
 		i++;
 	}
 
-	return (unsigned char)(lfs);
+	return u.u;
 }
 
 void main(void)
@@ -157,9 +167,15 @@ void main(void)
 	printf("初期値を入れてください = ");
 	scanf("%d", &n);
 	printf("%d\n", it(be(15)));
-	unsigned char l = lfsr(n);
+	unsigned long long l;
+	//unsigned char c= lfsr(n);
 
-	l = slf(l);
+	//while(1)
+	{
+	l = slf(n);
+	printf("%llu\n",l);
+	n+=l%0xffffffff;
+	}
 
 	return;
 }
