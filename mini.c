af8bebe2d8458685162d02f138025aefacf739f3
Osamu Sakai <fumumue@protonmail.com>
2023-05-21 22:33:11 +0900

    indent

--- mini.c
+++ mini.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <stdint.h>
 
-
 unsigned char c = 0b11000110;
 unsigned char v = 0b10100000;
 
@@ -40,13 +39,13 @@ int lfsr(unsigned char a)
 	unsigned char in_s, cs, cp, p, nbit, s[STREAM];
 	int i, j, k = 0;
 
-	in_s = a; //0xb4; /* this can be any 8 bit value */
-	p = 0x71;	 /* max length polynomial x^8+x^4+x^3+x^2+1 = 0b01110001 */
+	in_s = a; // 0xb4; /* this can be any 8 bit value */
+	p = 0x71; /* max length polynomial x^8+x^4+x^3+x^2+1 = 0b01110001 */
 
 	cs = in_s; /* copy initial state */
 
-	//printf("\nByte values for lfsr with initial value of 0xb4, and bit mask 0x71.\n");
-	//printf("Should correspond to primitive polynomial x^8+x^4+x^3+x^2+1.\n");
+	// printf("\nByte values for lfsr with initial value of 0xb4, and bit mask 0x71.\n");
+	// printf("Should correspond to primitive polynomial x^8+x^4+x^3+x^2+1.\n");
 
 	while (k < STREAM)
 	{
@@ -61,8 +60,8 @@ int lfsr(unsigned char a)
 			s[k] = cs & 0x01;
 			cs = (cs >> 1) | (nbit << 7); /*  rotate in new bit */
 		}
-		//printf(" %02x ", cs);
-		
+		// printf(" %02x ", cs);
+
 		return cs;
 		if (cs == in_s)
 		{
@@ -73,20 +72,18 @@ int lfsr(unsigned char a)
 
 static inline uint32_t rotl32(uint32_t x, int n)
 {
-  // http://blog.regehr.org/archives/1063
-  return x << n | (x >> (-n & 31));
+	// http://blog.regehr.org/archives/1063
+	return x << n | (x >> (-n & 31));
 }
 
-unsigned int
-xor (void)
-{
-  static unsigned int y = 2463534242;
-  y = y ^ (y << 13);
-  y = y ^ (y >> 17);
-  return y = y ^ (y << 15);
+unsigned int xor (void) {
+	static unsigned int y = 2463534242;
+	y = y ^ (y << 13);
+	y = y ^ (y >> 17);
+	return y = y ^ (y << 15);
 }
 
-int Shift(int e)
+	int Shift(int e)
 {
 	int shifted = (e << 1) ^ (((e & 0x80) != 0) ? 0x1B : 0x00);
 	return shifted;
@@ -132,22 +129,22 @@ unsigned char slf(unsigned char l)
 {
 	unsigned char lfs = l; // t=g(a)
 	unsigned char m = lfs;
-	unsigned int kount=0;
+	unsigned int kount = 0;
 	FILE *fp;
-	int i=1;
-	counti=be(lfs);
-	fp=fopen("test.bin","wb");
-	while (i<200000000)
+	int i = 1;
+	counti = be(lfs);
+	fp = fopen("test.bin", "wb");
+	while (i < 200000000)
 	{
-		unsigned char lfs2 = lfsr(lfs2^lfs);
-		
-		counti=rotl32(counti^it(lfs2),counti%32);
-		//printf("%d\n",counti>>1);
-		lfs ^= (p0w(loo(Dot(lfs,lfs2)), (counti>>1) + 1)); // s=(A^2r^2)^n
-		lfs ^= (Dot(lfs, (loo(m) ^ be(m) ^ c))); // s^n(A^2t+u) = s^n(A^2t+(At+c))
+		unsigned char lfs2 = lfsr(lfs2 ^ lfs);
+
+		counti = rotl32(counti ^ it(lfs2), counti % 32);
+		// printf("%d\n",counti>>1);
+		lfs ^= (p0w(loo(Dot(lfs, lfs2)), (counti >> 1) + 1)); // s=(A^2r^2)^n
+		lfs ^= (Dot(lfs, (loo(m) ^ be(m) ^ c)));			  // s^n(A^2t+u) = s^n(A^2t+(At+c))
 		++period;
-		//printf("%d %d\n", lfs, period);
-		fwrite(&lfs,sizeof(lfs),1,fp);
+		// printf("%d %d\n", lfs, period);
+		fwrite(&lfs, sizeof(lfs), 1, fp);
 		i++;
 	}
 
@@ -158,7 +155,7 @@ void main(void)
 {
 	int n;
 	printf("初期値を入れてください = ");
-	scanf("%d",&n);
+	scanf("%d", &n);
 	printf("%d\n", it(be(15)));
 	unsigned char l = lfsr(n);
 
