8a4d0845f3457b2748dc8bd2545e891c8a1e305e
Osamu Sakai <fumumue@protonmail.com>
2023-05-29 23:50:30 +0900

    perm

--- kpk.c
+++ kpk.c
@@ -4,14 +4,15 @@
 #include <string.h>
 #include <time.h>
 
-#define N 10
-#define Pr 11
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "global-p.h"
 #include "struct-p.h"
+
+#define N 16
+#define Pr 11
 
 #define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
 #define SWAP(type, a, b) \
@@ -57,38 +58,81 @@ void random_shuffle(unsigned *array, size_t size)
     }
 }
 
-typedef struct {
-short x[N];
-short y[N];
+typedef struct
+{
+    short x[N];
+    short y[N];
 } sem;
 
-sem semi(sem pi,sem tau){
-sem tmp={0};
-for(int i=0;i<N;i++){
-    pi.x[i]=i;
-    tau.x[i]=i;
-}
-random_shuffle(pi.x,N);
-random_shuffle(tau.x,N);
-for(int i=0;i<N;i++){
-pi.y[i]=rand()%256;
-tau.y[i]=random()%256;
-}
-for(int i=0;i<N;i++){
-tmp.x[i]=tau.x[pi.y[i]];
-tmp.x[i]+=pi.x[i];
-tmp.y[i]=pi.y[tau.y[i]];
-}
+sem semi(sem pi, sem tau)
+{
+    sem tmp = {0};
+    for (int i = 0; i < N; i++)
+    {
+        pi.x[i] = i;
+        tau.x[i] = i;
+    }
+    random_shuffle(pi.x, N);
+    random_shuffle(tau.x, N);
+    for (int i = 0; i < N; i++)
+    {
+        pi.y[i] = rand() % 256;
+        tau.y[i] = random() % 256;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        tmp.x[i] = tau.x[pi.y[i]];
+        tmp.x[i] += pi.x[i];
+        tmp.y[i] = pi.y[tau.y[i]];
+    }
 
-return tmp;
-}
-
-sem inv_sem(sem x){
-sem a={0};
-
-
+    return tmp;
 }
 
+sem inv_sem(sem x)
+{
+    sem a = {0};
+}
+
+void leo()
+{
+    unsigned P[16][N], Q[N], inv_Q[N] = {0}, R[N] = {0};
+    unsigned int z[N], y[N] = {0}, x[N] = {0}; //{137, 1832, 1763, 881, 1518, 3949, 3431, 2765, 2394, 1887, 2596, 3765, 3895, 4014, 3198, 656, 2354, 1930, 2251, 3834, 2953, 1706, 1411, 2725, 2651, 3595, 3940, 3376, 1452, 2292, 1291, 225};
+    unsigned r[N] = {0};
+    long long sabun = 0, s1 = 0, s3 = 0;
+    unsigned sys[N] = {0}, sys2[N], sys4[N];
+    unsigned pi[N] = {0}, inv_P[N], tau[N] = {0}, inv_tau[N] = {0};
+    unsigned char a = 0;
+    unsigned char aa[256][2560] = {0}, bb[256][256] = {0};
+    // srand(clock());
+    // chash();
+    // int j=7;
+    int j = 0;
+    for (int j = 0; j < 16; j++)
+    {
+        for (int i = 0; i < N; i++)
+            r[i] = P[j][i] = i;
+        random_shuffle(P[j], SIZE_OF_ARRAY(P[j]));
+    }
+    for (int k = 0; k < 16; k++)
+    {
+        for (int ii = 0; ii < N; ii++)
+            printf("%d,", P[k][ii]);
+        printf("\n");
+    }
+
+    // random_shuffle(Q, SIZE_OF_ARRAY(Q));
+    for (int k = 0; k < 2; k++)
+    {
+        for (int i = 0; i < N; i++)
+            x[i] = r[P[10][i]];
+        for (int j = 0; j < N; j++)
+            printf("%d,", r[j]);
+        printf("\n");
+        memcpy(r, x, sizeof(r));
+    }
+    return;
+}
 
 int main1()
 {
@@ -97,46 +141,65 @@ int main1()
     unsigned r[N] = {0};
     long long sabun = 0, s1 = 0, s3 = 0;
     unsigned sys[N] = {0}, sys2[N], sys4[N];
-    unsigned pi[N]={0},inv_P[N],tau[N]={0},inv_tau[N]={0};
-    unsigned char a=0;
-    unsigned char aa[256][2560]={0},bb[256][256]={0};
-    //srand(clock());
-    //chash();
-    //int j=7;
-    for(int i=0;i<N;i++)
+    unsigned pi[N] = {0}, inv_P[N], tau[N] = {0}, inv_tau[N] = {0};
+    unsigned char a = 0;
+    unsigned char aa[256][2560] = {0}, bb[256][256] = {0};
+    // srand(clock());
+    // chash();
+    // int j=7;
+    for (int i = 0; i < N; i++)
+        r[i] = Q[i] = P[i] = i;
+    random_shuffle(P, SIZE_OF_ARRAY(P));
+    random_shuffle(Q, SIZE_OF_ARRAY(Q));
+    for (int k = 0; k < 2; k++)
     {
-        int j=0;
-        a=i;
+        for (int i = 0; i < N; i++)
+            x[i] = r[P[i]];
+        memcpy(r, x, sizeof(r));
+        for (int j = 0; j < N; j++)
+            printf("%d,", x[j]);
+        printf("\n");
+    }
+    exit(1);
 
-        while(j<2560){
-        aa[i][j]=a;
-        printf("%d ",a);
-        //a=abox(a);
-        printf("%d\n",a);
-        j++;
+    for (int i = 0; i < N; i++)
+    {
+        int j = 0;
+        a = i;
+
+        while (j < 2560)
+        {
+            aa[i][j] = a;
+            printf("%d ", a);
+            // a=abox(a);
+            printf("%d\n", a);
+            j++;
         }
-        //printf("\n");
+        // printf("\n");
     }
 
-    
-    unsigned char tmp[2]={0};
-            int count=0;
-    for(int i=0;i<256;i++){
-        for(int j=0;j<2;j++)
-        tmp[j]=aa[i][j];
-        for(int k=0;k<256;k++){
-            for(int j=0;j<255;j++){
-        if(aa[k][j]==tmp[0] && aa[k][j+1]==tmp[1]){
-            //printf("%d %d %d\n",k,aa[k][j],aa[k][j+1]);
-            bb[k][0]++;
-            break;
+    unsigned char tmp[2] = {0};
+    int count = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        for (int j = 0; j < 2; j++)
+            tmp[j] = aa[i][j];
+        for (int k = 0; k < 256; k++)
+        {
+            for (int j = 0; j < 255; j++)
+            {
+                if (aa[k][j] == tmp[0] && aa[k][j + 1] == tmp[1])
+                {
+                    // printf("%d %d %d\n",k,aa[k][j],aa[k][j+1]);
+                    bb[k][0]++;
+                    break;
+                }
+            }
+            // bb[i][1]=tmp[1];
         }
-            }
-            //bb[i][1]=tmp[1];
-            }
     }
-    //for(int i=0;i<256;i++)
-    //printf("%d %d\n",i,bb[i][0]);
+    // for(int i=0;i<256;i++)
+    // printf("%d %d\n",i,bb[i][0]);
     exit(1);
 
     /*
@@ -146,25 +209,26 @@ int main1()
     }
     */
     for (int i = 0; i < N; i++)
-        tau[i]=Q[i] = P[i] = i;
+        tau[i] = Q[i] = P[i] = i;
     random_shuffle(P, SIZE_OF_ARRAY(P));
     random_shuffle(Q, SIZE_OF_ARRAY(Q));
     random_shuffle(tau, SIZE_OF_ARRAY(tau));
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++)
+    {
         inv_Q[Q[i]] = i;
-        inv_P[P[i]]=i;
-        inv_tau[tau[i]]=i;
-        printf("%d,",tau[i]);
-        }
-        //exit(1);
+        inv_P[P[i]] = i;
+        inv_tau[tau[i]] = i;
+        printf("%d,", tau[i]);
+    }
+    // exit(1);
     unsigned t[N] = {0};
     for (int i = 0; i < N; i++)
     {
-        R[tau[i]]=i; //inv_tau
-        t[i] = inv_P[tau[i]]; //tau*pi^{-1}
+        R[tau[i]] = i;        // inv_tau
+        t[i] = inv_P[tau[i]]; // tau*pi^{-1}
         printf("%d,%d\n", R[Q[i]], tau[i]);
     }
-    unsigned w[N]={0};
+    unsigned w[N] = {0};
     printf("\n");
     // exit(1);
     for (int j = 0; j < N; j++)
@@ -172,35 +236,36 @@ int main1()
         sabun = 0;
         s1 = 0;
         s3 = 0;
-        unsigned rr[N]={0};
+        unsigned rr[N] = {0};
         for (int i = 0; i < N; i++)
         {
             // sabun += (x[P[i]] ^ inv(x[(i + 1) % N],N));
-            y[i] = x[P[i]]; //x_pi
-            w[i] =x[P[inv_tau[P[i]]]]; //x_{P*inv_tau*P}
-            z[i] = r[tau[i]]; //y_pi
-            rr[i]=r[P[i]];
-            //printf("%d,",t[i]);
+            y[i] = x[P[i]];             // x_pi
+            w[i] = x[P[inv_tau[P[i]]]]; // x_{P*inv_tau*P}
+            z[i] = r[tau[i]];           // y_pi
+            rr[i] = r[P[i]];
+            // printf("%d,",t[i]);
         }
-        //exit(1);
-        //memcpy(x, y, sizeof(x));
-        //memcpy(r, z, sizeof(r));
-        unsigned tt[N]={0};
-        unsigned s[N]={0};
-        unsigned u[N]={0};
+        // exit(1);
+        // memcpy(x, y, sizeof(x));
+        // memcpy(r, z, sizeof(r));
+        unsigned tt[N] = {0};
+        unsigned s[N] = {0};
+        unsigned u[N] = {0};
         for (int i = 0; i < N; i++)
-            tt[i] = w[i]^rr[i]; //z
-        for(int i=0;i<N;i++){
-            s[i]=tt[t[i]]; //z_{tau*pi^{-1}} 
-            ///printf("%d,",s[i]);
-            }
-            for(int i=0;i<N;i++)
-            s[i]^=y[i];
-            //printf("\n");
-            //exit(1);
+            tt[i] = w[i] ^ rr[i]; // z
         for (int i = 0; i < N; i++)
         {
-            printf("%d %d\n",s[i],z[i]);
+            s[i] = tt[t[i]]; // z_{tau*pi^{-1}}
+            /// printf("%d,",s[i]);
+        }
+        for (int i = 0; i < N; i++)
+            s[i] ^= y[i];
+        // printf("\n");
+        // exit(1);
+        for (int i = 0; i < N; i++)
+        {
+            printf("%d %d\n", s[i], z[i]);
             sabun += s[i] ^ y[(i)];
             s1 += (r[i] ^ r[(i + 1) % N]);
             s3 += t[i];
@@ -211,10 +276,9 @@ int main1()
         sys4[j] = s3 % Pr;
     }
     for (int i = 0; i < N; i++)
-        printf("%d,%d %d %d\n", sys[i], sys2[i], (sys[i]+sys2[i])%Pr, sys4[i]);
+        printf("%d,%d %d %d\n", sys[i], sys2[i], (sys[i] + sys2[i]) % Pr, sys4[i]);
     printf("\n");
 }
-
 
 int main()
 {
@@ -223,16 +287,20 @@ int main()
     unsigned xx[N] = {0};
     unsigned r[N] = {0};
     unsigned s[N] = {0}, w[N] = {0};
-    long long sabun = 0, s1 = 0, s3 = 0,s4=0;
-    unsigned sys[N] = {0}, sys2[N]={0}, sys4[N]={0},sys1[N]={0};
+    long long sabun = 0, s1 = 0, s3 = 0, s4 = 0;
+    unsigned sys[N] = {0}, sys2[N] = {0}, sys4[N] = {0}, sys1[N] = {0};
 
     srand(clock());
-    for (int i = 0; i < N; i++){
+    leo();
+    exit(1);
+
+    for (int i = 0; i < N; i++)
+    {
         Q[i] = P[i] = i;
-        xx[i]=rand()%Pr;
-        printf("%d,",xx[i]);
+        xx[i] = rand() % Pr;
+        printf("%d,", xx[i]);
     }
-    //exit(1);
+    // exit(1);
     random_shuffle(P, SIZE_OF_ARRAY(P));
     random_shuffle(Q, SIZE_OF_ARRAY(Q));
     for (int i = 0; i < N; i++)
@@ -250,66 +318,65 @@ int main()
     {
         printf("%d,", P[i]);
         s[i] = rand() % Pr;
-        r[i]=s[i]+xx[i];
+        r[i] = s[i] + xx[i];
         v[i] = xx[P[i]];
     }
     printf("\n");
-        unsigned t[N] = {0};
+    unsigned t[N] = {0};
     for (int i = 0; i < N; i++)
         t[i] = s[Q[i]];
-        /*
-    for (int i = 0; i < N; i++)
-    {
-        r[i] = ((s[i] ^ s[(i + 1) % N]) + (x[i] ^ x[(i + 1) % N]));
-        printf("%d %d\n", r[i], ((s[i] ^ s[(i + 1) % N]) + (x[i] ^ x[(i + 1) % N])));
-    }
-    printf("\n");
-    //exit(1);
+    /*
+for (int i = 0; i < N; i++)
+{
+    r[i] = ((s[i] ^ s[(i + 1) % N]) + (x[i] ^ x[(i + 1) % N]));
+    printf("%d %d\n", r[i], ((s[i] ^ s[(i + 1) % N]) + (x[i] ^ x[(i + 1) % N])));
+}
+printf("\n");
+//exit(1);
 */
-    unsigned x[N]={0};
+    unsigned x[N] = {0};
     for (int j = 0; j < N; j++)
     {
         sabun = 0;
         s1 = 0;
         s3 = 0;
-        s4=0;
-        for (int i = 0; i < N-1; i++)
+        s4 = 0;
+        for (int i = 0; i < N - 1; i++)
         {
-            s3 += (s[i]^s[(i+1)%N])+(xx[i]^xx[(i+1)%N]);
-            s4 += (r[i]+r[(i+1)]);
-            //sabun += (xx[P[i]] ^ xx[(i + 1) % N]);
+            s3 += (s[i] ^ s[(i + 1) % N]) + (xx[i] ^ xx[(i + 1) % N]);
+            s4 += (r[i] + r[(i + 1)]);
+            // sabun += (xx[P[i]] ^ xx[(i + 1) % N]);
             sabun += (xx[i] + xx[(i + 1)]);
-            //printf("%d,%d\n",j,xx[i]);
+            // printf("%d,%d\n",j,xx[i]);
             s1 += (s[i] + s[(i + 1)]);
-            
-            //sabun%=Pr;
-            //s1%=Pr;
-            //s4%=Pr;
-            //s3%=Pr;
+
+            // sabun%=Pr;
+            // s1%=Pr;
+            // s4%=Pr;
+            // s3%=Pr;
 
             y[i] = xx[P[i]];
             z[i] = r[P[i]];
             w[i] = s[P[i]];
-            
         }
         memcpy(xx, y, sizeof(xx));
         memcpy(r, z, sizeof(Z));
         memcpy(s, w, sizeof(w));
-        sys[j] = (sabun);// % Pr;
-        sys2[j] = s1;// % Pr;
-        sys4[j] = s4%Pr; //sys[j]+sys2[j] % Pr;
+        sys[j] = (sabun);  // % Pr;
+        sys2[j] = s1;      // % Pr;
+        sys4[j] = s4 % Pr; // sys[j]+sys2[j] % Pr;
     }
-    //exit(1);
+    // exit(1);
     unsigned u[N] = {0};
-    //for (int i = 0; i < N; i++)
-    //    u[i] = t[R[i]];
+    // for (int i = 0; i < N; i++)
+    //     u[i] = t[R[i]];
     for (int i = 0; i < N; i++)
-        printf("%d ", (sys[i]+sys2[i])%Pr ); //((t[i]+t[(i+1)%N]) ^ (x[i]+x[(i+1)%N]))%Pr);
+        printf("%d ", (sys[i] + sys2[i]) % Pr); //((t[i]+t[(i+1)%N]) ^ (x[i]+x[(i+1)%N]))%Pr);
     printf("\n");
-    //for (int i = 0; i < N; i++)
-    //    printf("%d ", (sys2[i])); /// sys2[i],sys[i],sys4[i]);
-    //printf("\n");
+    // for (int i = 0; i < N; i++)
+    //     printf("%d ", (sys2[i])); /// sys2[i],sys[i],sys4[i]);
+    // printf("\n");
     for (int i = 0; i < N; i++)
-        printf("%d ", sys4[i] );
+        printf("%d ", sys4[i]);
     printf("\n");
 }
